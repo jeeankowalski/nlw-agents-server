120cbe484b29313dbf81b71eac06465511bccab1
Jean <jean@exciasistemas.com.br>
2025-07-09 17:56:00 -0300

    feat: added gemini integration; upload audio;

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,21 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
-
+import { fastifyMultipart } from '@fastify/multipart';
 import { env } from './env.ts';
 import { createQuestionRoute } from './http/routes/create-question.ts';
 import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomQuestions } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { uploadAudioRoute } from './http/routes/upload-audio.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 });
+
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -29,5 +32,6 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestions);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
